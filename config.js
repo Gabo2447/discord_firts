066023785c9091b02661b8af99cc5b8d960d0443
Gabo2447 <gabrieldeolival2@gmail.com>
2025-04-07 00:52:31 -0400

    [fix]: security brench

--- config.js
+++
@@ -1,9 +0,0 @@
-const token = "MTM1NjQxNzk2NzQyMzg4MTM1OA.GKMMaM.iO20QOQFh79txg0QyDUJsO8HsFHz4p1cs3yJU0"
-const prefix ="?"
-const clientId = BigInt(1356417967423881358)
-
-export default {
-  token,
-  prefix,
-  clientId
-}
